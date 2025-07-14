d0343833643604c3edee974db96afb41edccc421
Barış <bayazitbaris@gmail.com>
2025-07-13 23:49:48 -0400

    simple example

--- Example.v
+++ Example.v
@@ -1,69 +1,32 @@
 Require Import Vevm.Vm.
 Require Import Vevm.Instruction.
+Require Import Vevm.VmProofs.
 
 From Stdlib Require Import List.
+From Stdlib Require Import Lia.
+
 Import ListNotations.
 
-(*
-
-result = 0
-i = 0
-while i != 5:
-    result += i
-    i += 1
-return result
-
-*)
-Definition loop_sum_to_n (n: nat) : list Instruction :=
-  [
-    IPush 0; IPush 0; IMstore;
-    IPush 0; IPush 1; IMstore;
-
-    IPush 1; IMload;
-    IPush (n + 1); IEq;
-    IPush 29; ISwap; IJmpi;
-
-    IPush 0; IMload;
-    IPush 1; IMload;
+Definition two_n_plus_eight_program (n: nat) : list Instruction := [
+    IPush n;
+    IPush n;
     IAdd;
-    IPush 0; IMstore;
+    IPush 8;
+    IAdd;
+    IOutput
+].
 
-    IPush 1; IMload;
-    IPush 1; IAdd;
-    IPush 1; IMstore;
+Definition two_n_plus_eight (n: nat) := 2 * n + 8.
 
-    IPush 6; IPush 1; IJmpi;
+Compute output (interpret_all_with_fuel (two_n_plus_eight_program 24) 6).
 
-    IPush 0; IMload; IOutput
-  ].
-
-Definition sum_up_to_n n := Nat.div (n * (n + 1)) 2.
-
-(* Compute ((interpret_all_with_fuel (loop_sum_to_n 0) 39)).
-Compute (output (interpret_all_with_fuel (loop_sum_to_n 1) 62)).
-Compute (output (interpret_all_with_fuel (loop_sum_to_n 2) 85)).
-Compute (output (interpret_all_with_fuel (loop_sum_to_n 3) 108)).
-Compute (output (interpret_all_with_fuel (loop_sum_to_n 5) 154)).
-Compute (output (interpret_all_with_fuel (loop_sum_to_n 6) 177)).
-Compute (output (interpret_all_with_fuel (loop_sum_to_n 40) 959)).
-Compute sum_up_to_n 40. *)
-
-(* min fuel amount := 39 + 23n *)
-
-Theorem loop_sum_to_n_correctness :
+Theorem two_n_plus_eight_correctness :
   forall (n: nat), exists (fuel: nat),
-    output (interpret_all_with_fuel (loop_sum_to_n n) fuel) = sum_up_to_n n.
+    output (interpret_all_with_fuel (two_n_plus_eight_program n) fuel) = two_n_plus_eight n.
 Proof.
   intros n.
-
-  induction n as [| n IHn].
-  - exists (39 + 23 * 0).
-    compute.
-    reflexivity.
-  - destruct IHn as [fuel_n fuel_IHn].
-    exists (fuel_n + 23).
-    
-
-Admitted.
-
-
+  exists 6.
+  simpl.
+  unfold two_n_plus_eight.
+  lia.
+Qed.
