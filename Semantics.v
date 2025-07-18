bb2e5565d78135c332e457c55d17b6d0d07cb641
Barış <bayazitbaris@gmail.com>
2025-07-18 16:49:49 -0400

    Capture halted in state

--- Semantics.v
+++ Semantics.v
@@ -11,58 +11,58 @@ Inductive step : VmState -> list Instruction -> VmState -> Prop :=
     | SIPush : forall stack pc memory instructions n,
         List.nth_error instructions pc = Some (IPush n) ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := n :: stack; pc := pc + 1; memory := memory |}
+            {| stack := n :: stack; pc := pc + 1; memory := memory; halted := false |}
 
     | SIMstore : forall stack pc memory instructions offset value stack',
         List.nth_error instructions pc = Some IMstore ->
         stack = offset :: value :: stack' ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := stack'; pc := pc + 1; memory := NatMap.add offset value memory |}
+            {| stack := stack'; pc := pc + 1; memory := NatMap.add offset value memory; halted := false |}
 
     | SIMload : forall stack pc memory instructions offset stack' found,
         List.nth_error instructions pc = Some IMload ->
         stack = offset :: stack' ->
         NatMap.find offset memory = Some found ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := found :: stack'; pc := pc + 1; memory := memory |}
+            {| stack := found :: stack'; pc := pc + 1; memory := memory; halted := false |}
 
     | SIAdd : forall stack pc memory instructions a b stack',
         List.nth_error instructions pc = Some IAdd ->
         stack = a :: b :: stack' ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := (a + b) :: stack'; pc := pc + 1; memory := memory |}
+            {| stack := (a + b) :: stack'; pc := pc + 1; memory := memory; halted := false |}
 
     | SIOutput : forall stack pc memory instructions a rest,
         List.nth_error instructions pc = Some IOutput ->
         stack = a :: rest ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := [a]; pc := pc + 1; memory := memory |}
+            {| stack := [a]; pc := pc; memory := memory; halted := true |}
 
     | SIEq : forall stack pc memory instructions a b stack',
         List.nth_error instructions pc = Some IEq ->
         stack = a :: b :: stack' ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := (if Nat.eqb a b then 1 else 0) :: stack'; pc := pc + 1; memory := memory |}
+            {| stack := (if Nat.eqb a b then 1 else 0) :: stack'; pc := pc + 1; memory := memory; halted := false |}
 
     | SIJmpiTrue : forall stack pc memory instructions address stack',
         List.nth_error instructions pc = Some IJmpi ->
         stack = 1 :: address :: stack' ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := stack'; pc := address; memory := memory |}
+            {| stack := stack'; pc := address; memory := memory; halted := false |}
 
     (* ONLY if should_jump != 1; not just 0, although 0 is probably
     the most common way to do this. I included this because simplify
@@ -74,33 +74,33 @@ Inductive step : VmState -> list Instruction -> VmState -> Prop :=
         stack = should_jump :: address :: stack' ->
         should_jump <> 1 ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := stack'; pc := pc + 1; memory := memory |}
+            {| stack := stack'; pc := pc + 1; memory := memory; halted := false |}
 
     | SIDup : forall stack pc memory instructions a stack',
         List.nth_error instructions pc = Some IDup ->
         stack = a :: stack' ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := a :: a :: stack'; pc := pc + 1; memory := memory |}
+            {| stack := a :: a :: stack'; pc := pc + 1; memory := memory; halted := false |}
 
     | SIPop : forall stack pc memory instructions a stack',
         List.nth_error instructions pc = Some IPop ->
         stack = a :: stack' ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := stack'; pc := pc + 1; memory := memory |}
+            {| stack := stack'; pc := pc + 1; memory := memory; halted := false |}
 
     | SISwap : forall stack pc memory instructions a b stack',
         List.nth_error instructions pc = Some ISwap ->
         stack = a :: b :: stack' ->
         step
-            {| stack := stack; pc := pc; memory := memory |}
+            {| stack := stack; pc := pc; memory := memory; halted := false |}
             instructions
-            {| stack := b :: a :: stack'; pc := pc + 1; memory := memory |}
+            {| stack := b :: a :: stack'; pc := pc + 1; memory := memory; halted := false |}
 .
 
 Inductive steps : VmState -> list Instruction -> VmState -> Prop :=
--- Vm.v
+++ Vm.v
@@ -14,7 +14,8 @@ Module NatMap := FMapAVL.Make(Nat_as_OT).
 Record VmState := {
     stack: list nat;
     pc: nat;
-    memory : NatMap.t nat
+    memory : NatMap.t nat;
+    halted: bool;
 }.
 
 Inductive VmResult :=
@@ -36,49 +37,49 @@ Definition output (result: VmResult) : nat := match result with
 
 Definition interpret (instr : Instruction) (state: VmState) : VmResult :=
     match instr with
-        | IPush n => Ok {| stack := n :: state.(stack) ; pc := state.(pc) + 1; memory := state.(memory) |}
+        | IPush n => Ok {| stack := n :: state.(stack) ; pc := state.(pc) + 1; memory := state.(memory); halted := false |}
         | IMstore => match state.(stack) with
             | offset :: value :: s' => Ok {|
-                stack := s' ; pc := state.(pc) + 1; memory := (NatMap.add offset value state.(memory))
+                stack := s' ; pc := state.(pc) + 1; memory := (NatMap.add offset value state.(memory)); halted := false;
             |}
             | _ => Err "mstore failed"
             end
         | IMload => match state.(stack) with
             | offset :: s' =>
                 match NatMap.find offset state.(memory) with
-                    | Some found => Ok {| stack := found :: s'; pc := state.(pc) + 1; memory := state.(memory) |}
+                    | Some found => Ok {| stack := found :: s'; pc := state.(pc) + 1; memory := state.(memory); halted := false |}
                     | None => Err "failed to find key"
                 end
             | _ => Err "failed to find key"
             end
         | IAdd => match state.(stack) with
-            | a :: b :: s' => Ok {| stack := (a + b) :: s' ; pc := state.(pc) + 1; memory := state.(memory) |}
+            | a :: b :: s' => Ok {| stack := (a + b) :: s' ; pc := state.(pc) + 1; memory := state.(memory); halted := false |}
             | _ => Err "add failed"
             end
         | IOutput => match state.(stack) with
-            | a :: rest' => Ok {| stack := [a] ; pc := state.(pc) + 1; memory := state.(memory) |}
+            | a :: rest' => Ok {| stack := [a] ; pc := state.(pc); memory := state.(memory); halted := true |}
             | _ => Err "output failed"
             end
         | IEq => match state.(stack) with
-            | a :: b :: s' => Ok {| stack := (if Nat.eqb a b then 1 else 0) :: s' ; pc := state.(pc) + 1; memory := state.(memory) |}
+            | a :: b :: s' => Ok {| stack := (if Nat.eqb a b then 1 else 0) :: s' ; pc := state.(pc) + 1; memory := state.(memory); halted := false |}
             | _ => Err "eq failed"
             end
         | IJmpi => match state.(stack) with
             | should_jump :: address :: s' => if Nat.eqb should_jump 1 then
-                Ok {|stack := s'; pc := address; memory := state.(memory) |}
-                else Ok {| stack := s'; pc := state.(pc) + 1; memory := state.(memory) |}
+                Ok {|stack := s'; pc := address; memory := state.(memory); halted := false |}
+                else Ok {| stack := s'; pc := state.(pc) + 1; memory := state.(memory); halted := false |}
             | _ => Err "jmp failed"
             end
         | IDup => match state.(stack) with
-            | a :: s' => Ok {| stack := (a :: (a :: s')) ; pc := state.(pc) + 1; memory := state.(memory) |}
+            | a :: s' => Ok {| stack := (a :: (a :: s')) ; pc := state.(pc) + 1; memory := state.(memory); halted := false |}
             | _ => Err "dup failed"
             end
         | IPop => match state.(stack) with
-            | a :: s' => Ok {| stack := s'; pc := state.(pc) + 1; memory := state.(memory) |}
+            | a :: s' => Ok {| stack := s'; pc := state.(pc) + 1; memory := state.(memory); halted := false |}
             | _ => Err "nothing to pop"
             end
         | ISwap => match state.(stack) with
-            | a :: b :: s' => Ok {| stack := (b :: (a :: s')) ; pc := state.(pc) + 1; memory := state.(memory) |}
+            | a :: b :: s' => Ok {| stack := (b :: (a :: s')) ; pc := state.(pc) + 1; memory := state.(memory); halted := false |}
             | _ => Err "swap failed"
             end
 end.
@@ -87,27 +88,30 @@ Fixpoint interpret_all' (instrs: list Instruction) (state: VmState) (fuel: nat) 
     match fuel with
     | 0 => Err "out of fuel"
     | S fuel' =>
-        match List.nth_error instrs state.(pc) with
-            | None => Err "pc out of bounds"
-            | Some IOutput =>
-             match interpret IOutput state with
-                | Err e     => Err e
-                | Ok state' => Ok state'
-             end
-            | Some instr => match interpret instr state with
-                | Err e => Err e
-                | Ok state' => interpret_all' instrs state' fuel'
+        if state.(halted) then
+            Ok state
+        else
+            match List.nth_error instrs state.(pc) with
+                | None => Err "pc out of bounds"
+                | Some IOutput =>
+                match interpret IOutput state with
+                    | Err e     => Err e
+                    | Ok state' => Ok state'
+                end
+                | Some instr => match interpret instr state with
+                    | Err e => Err e
+                    | Ok state' => interpret_all' instrs state' fuel'
+                end
             end
-        end
     end.
 
 
 Definition interpret_all (instrs: list Instruction): VmResult :=
-    interpret_all' instrs {| stack := []; pc := 0 ; memory := NatMap.empty nat |} 1000.
+    interpret_all' instrs {| stack := []; pc := 0 ; memory := NatMap.empty nat; halted := false |} 1000.
 
 
 Definition interpret_all_with_fuel (instrs: list Instruction) (fuel: nat): VmResult :=
-    interpret_all' instrs {| stack := []; pc := 0 ; memory := NatMap.empty nat |} fuel.
+    interpret_all' instrs {| stack := []; pc := 0 ; memory := NatMap.empty nat; halted := false |} fuel.
 
 
 (* Require Extraction.
